7060ef2cb363169547a1c243d321e3bfbe20da1b
Twinkal Paralkar <twinkalparalkar2@gmail.com>
2024-04-11 01:43:34 +0530

    Exploring Regular Expression

---
+++ Welcome.js
@@ -0,0 +1,11 @@
+alert("Pop Up")
+console.log("print my message")
+///////// Regular expression
+char=5
+if (/[A-Z]/.test(char)) {
+    console.log(1)
+} else if (/[a-z]/.test(char)) {
+    console.log(0)
+} else {
+    console.log(-1)
+}
