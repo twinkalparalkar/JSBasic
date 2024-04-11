021b1feee5e92778e568c2df3517af963bc24b91
Twinkal Paralkar <twinkalparalkar2@gmail.com>
2024-04-11 13:13:06 +0530

    Exploring Data type size in JS

--- Welcome.js
+++ Welcome.js
@@ -9,3 +9,22 @@ if (/[A-Z]/.test(char)) {
 } else {
     console.log(-1)
 }
+// regular expression :-is used when we want pattern (we can't use toUpperCase() method)
+// if(x===x.toUpperCase()){ return 1;} 
+function getDataTypeSize(dataType) {
+    switch(dataType.toLowerCase()) 
+    {
+        case 'integer':
+            return 4; // Assuming 4 bytes for integer
+        case 'long':
+            return 8; // Assuming 8 bytes for long
+        case 'float':
+            return 4; // Assuming 4 bytes for float
+        case 'double':
+            return 8; // Assuming 8 bytes for double
+        case 'character':
+            return 1; // Assuming 1 byte for character
+        default:
+            return -1; // Invalid data type
+    }
+}
