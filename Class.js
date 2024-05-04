037a0a331cd480cc23703130743ad1ab16124e63
twinkalp <twinkalparalkar2@gmail.com>
2024-05-04 21:01:31 +0530

    Class and constructor in JS

--- Class.js
+++ Class.js
@@ -45,7 +45,25 @@ const object1={attritube1:"prototype"}
 const object2={attritube1:"original property"}
 object2.__proto__=object1 
 console.log(object2,object2.attritube1)
+//Class in JS :-always have default constructor for initialization in prototype
+class Car{
+    constructor(speed,type){//custom constructor for pasing argument
+        this.speed=speed
+        this.brandType=type 
+        console.log("creating new object")
+    }
+    start(){
+        console.log("start Car")
+    }
+    stop(time){
+        console.log(time)
+    }
+}
+let obj1=new Car(39,"TVS")//this argument for constructor
+obj1.stop(49)
+obj1.brandType="RD"//change object property
+console.log(obj1)
 
-
+//Inheritance
 
 
