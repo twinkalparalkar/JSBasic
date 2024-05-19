1ee3a9196d8dd96c15b00ee5935504ed6caeebbe
twinkalp <twinkalparalkar2@gmail.com>
2024-05-19 13:23:56 +0530

    Updating Quote Generator Website in JS

--- QuoteGenerator/logic.js
+++ QuoteGenerator/logic.js
@@ -1,3 +1,6 @@
+let quote=document.querySelector(".quote")
+let author=document.querySelector(".author")
+
 const getData=async ()=>{
 let data=await fetch("https://api.quotable.io/quotes/random?minLength=100&maxLength=140")
 console.log(data)
@@ -10,6 +13,16 @@ console.log(QuoteDetail,QuoteDetail[0].author,QuoteDetail[0].content)
 window.addEventListener('load',()=>{
     updateQuote()
 })
-const updateQuote=()=>{
-    
+
+document.querySelector("#newQuoteButton").addEventListener("click",()=>{
+    updateQuote()
+})
+
+const updateQuote=async ()=>{
+    let data=await fetch("https://api.quotable.io/quotes/random?minLength=100&maxLength=140")
+    console.log(data)
+    let QuoteDetail=await data.json()
+    console.log(QuoteDetail,QuoteDetail[0].author,QuoteDetail[0].content)
+    quote.innerText=QuoteDetail[0].content
+    author.innerText="-"+QuoteDetail[0].author
 }
